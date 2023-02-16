6815a42b8ebbecff8b5c8dd90634421819f21e0f
Diwash Rai <raidiwash30@gmail.com>
2023-02-16 20:14:01 +0545

    new comment

--- lib/Screen/home_page.dart
+++ lib/Screen/home_page.dart
@@ -56,6 +56,7 @@ class _HomePageState extends State<HomePage> {
                     )),
               ),
               Consumer<DataProvider>(
+                  //consumer data
                   builder: (context, value, child) => Padding(
                       padding: const EdgeInsets.all(8.0),
                       child: TextWidget(
